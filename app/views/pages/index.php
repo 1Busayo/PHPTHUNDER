d83d74205520f29a265889be3d7494722391a169
unknown <fanimokunbusayo@gmail.com>
2020-08-15 20:33:53 +0100

    first commit

---
+++ app/views/pages/index.php
@@ -0,0 +1,6 @@
+<?php require APPROOT.'/views/inc/header.php'; ?>
+<h1> <?php echo $data['title']; ?></h1>
+
+<p>This a php framework please contact 1Busayo on github to build your personal php framework </p>
+<?php require APPROOT.'/views/inc/footer.php'; ?>
+
